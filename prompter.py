d2b7c2ce06f78ce8cf4896d7939842ec5e83db30
Faraz Murshed <farazmurshed@gmail.com>
2021-01-30 13:29:39 -0800

    Initial commit

---
+++ prompter.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+app = Flask(__name__)
+
+# handle incoming sms
+@app.route("/sms", methods=['GET', 'POST'])
+def handle_sms():
+    # Get the message the user sent to Twilio number
+    body = request.values.get('Body', None)
+
+    # configure response message
+    res = "hello"
+
+    # TwiML response
+    resp = MessagingResponse()
+    resp.message(res)
+
+    return str(resp)
+
+
+
+
+# run flask app
+if __name__ == '__main__':
+    app.run()
+
+
