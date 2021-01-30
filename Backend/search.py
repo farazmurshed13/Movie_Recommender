b3cfebe8094de7ad93dfd8ea05da31fa40dfada4
Ryan Lam <ryan.lam53@gmail.com>
2021-01-30 15:42:38 -0800

    Regex searching

--- Backend/search.py
+++ Backend/search.py
@@ -1,5 +1,6 @@
 import pymongo
 import urllib
+from pprint import pprint
 
 client = pymongo.MongoClient("mongodb+srv://ryan:" + urllib.parse.quote_plus("7926COAco87") + "@cluster0.zmj8z.mongodb.net/movies?retryWrites=true&w=majority")
 
@@ -7,8 +8,8 @@ db = client['mydatabase']
 
 movies = db['movies']
 
-query = {'original_title': 'Gemini Man', 'country': {"$regex":"\^USA"}}
+query = {'original_title': 'Gemini Man', 'country': {"$regex": ".*USA.*"}}
 
-res = movies.find({'country': "USA"})
+res = movies.find(query)
 
-print(res[0])
+pprint(res[0])
