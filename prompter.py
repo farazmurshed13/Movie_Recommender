9bd689a19939016aa7bef714938fced9b129f3bc
Faraz Murshed <farazmurshed@gmail.com>
2021-01-30 18:04:41 -0800

    test web

--- prompter.py
+++ prompter.py
@@ -5,7 +5,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 # handle incoming sms
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/sms", methods=['GET', 'POST'])
 def handle_sms():
     # Get the message the user sent to Twilio number
     body = request.values.get('Body', None)
@@ -16,6 +16,9 @@ def handle_sms():
 
     return str(resp)
 
+@app.route("/")
+def site():
+    return "hello"
 
 # run flask app
 if __name__ == '__main__':
