a4a64efcd184b919e146a582ccef5c9b3a639a29
Faraz Murshed <farazmurshed@gmail.com>
2021-01-31 08:58:24 -0800

    debug

--- prompter.py
+++ prompter.py
@@ -106,16 +106,32 @@ def handle_sms():
             b = info[1] / info[4]
             r = info[2] / info[4]
             f = info[3] / info[4]
-            movie1 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
-            movie2 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
-            movie3 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
-            movie4 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
-            movie5 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
-            movies_list = [movie1, movie2, movie3, movie4, movie5]
 
-            #msg = str(t) + str(b) + str(r) + str(f) + info[5] + info[6] + info[7]
-            #msg = sc.movie_msg(ml)
-            msg = movie1 + movie2 + movie3 + movie4 + movie5
+            # get movies
+            movies_list = []
+            movie1 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
+            msg += (movie1 + "\n")
+            movies_list.append(movie1)
+
+            movie2 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
+            if movie2 not in movies_list:
+                movies_list.append(movie2)
+                msg += (movie2 + "\n")
+
+            movie3 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
+            if movie3 not in movies_list:
+                movies_list.append(movie3)
+                msg += (movie3 + "\n")
+
+            movie4 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
+            if movie4 not in movies_list:
+                movies_list.append(movie4)
+                msg += (movie4 + "\n")
+
+            movie5 = mc.generateMovList(t, b, r, f, info[5], info[6], info[7])
+            if movie5 not in movies_list:
+                movies_list.append(movie5)
+                msg += (movie5 + "\n")
         else:
             msg = "done - waiting for other users!"
 
