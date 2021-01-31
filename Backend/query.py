9c5d3122f859e552e654b67e0642b78e24c2098f
articbear1999 <jacobchenlin@gmail.com>
2021-01-30 16:05:21 -0800

    get all genre's of movies

--- Backend/query.py
+++ Backend/query.py
@@ -1,12 +1,19 @@
 import pymongo
+from pprint import pprint
 client = pymongo.MongoClient("mongodb+srv://articbear1999:Jacoblin1!@cluster0.zmj8z.mongodb.net/movies?retryWrites=true&w=majority")
 db = client['mydatabase']
 
 movies = db['movies']
 
-myquery = { "title": "Miss Jerry" }
-
-mydoc = movies.find(myquery)
+mydoc = movies.find({},{ "genre":1, "_id":False})
+mySet = set()
 
 for x in mydoc:
-  print(x) 
+    splitStr = x['genre'].split()
+    for genre in splitStr:
+        genre = genre.replace(',','')
+        if genre in mySet:
+            continue
+        print(genre)
+        mySet.add(genre)
+print(len(mySet))
