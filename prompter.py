0a515b0b853636cac820783035d76d4e199da6e8
Ryan Lam <ryan.lam53@gmail.com>
2021-01-30 18:14:20 -0800

    Created HTML files

--- prompter.py
+++ prompter.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
 # handle incoming sms
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/sms", methods=['GET', 'POST'])
 def handle_sms():
     # Get the message the user sent to Twilio number
     body = request.values.get('Body', None)
