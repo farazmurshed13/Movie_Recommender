a410b085fcf9ecd8c46112202cff8de824bd0424
Faraz Murshed <farazmurshed@gmail.com>
2021-01-30 20:08:41 -0800

    test back-and-forth comm

--- prompter.py
+++ prompter.py
@@ -9,23 +9,41 @@ app.config.from_object(__name__)
 # handle incoming sms
 @app.route("/sms", methods=['GET', 'POST'])
 def handle_sms():
-    # count messages in session
-    counter = session.get('counter', 0)
-    counter += 1
-    if counter >= 2:
-        session['counter'] = 0
-    else:
-        session['counter'] = counter
-
-    from_number = request.values.get('From')
+    # store incoming message
+    # from_number = request.values.get('From')
     incoming = request.values.get('Body', None)
 
-    # Get the message the user sent to Twilio number
-    message = '{} was messaged by {}; {} total msg.' \
-        .format(incoming, from_number, counter)
+    # count messages in session
+    counter = session.get('counter', 0)
 
+    # determine which question to ask
+    # store party code and ask q1
+    if counter == 0:
+        party_code = incoming
+        msg = "question 1"
+    # store r1 and ask q2
+    elif counter == 1:
+        msg = "question 2"
+    # store r2 and ask q3
+    elif counter == 2:
+        msg = "question 3"
+    # store r3 and ask q4
+    elif counter == 3:
+        msg = "question 4"
+    # store r4
+    else:
+        msg = "done!"
+
+    # increment or reset counter
+    if counter > 3:
+        counter = 0
+    else:
+        counter += 1
+    session['counter'] = counter
+
+    # reply to user
     outgoing = MessagingResponse()
-    outgoing.message(message)
+    outgoing.message(msg)
 
     return str(outgoing)
 
