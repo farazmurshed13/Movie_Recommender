f8e38007aab6871d3e44d2e4297920f56e50a030
Faraz Murshed <farazmurshed@gmail.com>
2021-01-30 22:25:52 -0800

    test party codes

--- Backend/store_conversation.py
+++ Backend/store_conversation.py
@@ -3,7 +3,7 @@ import urllib
 from random import randint
 
 # set up mongo db client
-client = pymongo.MongoClient("mongodb+srv://ryan:" + urllib.parse.quote_plus("7926COAco87") + "@cluster0.zmj8z.mongodb.net/movies?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://ryan:" + urllib.parse.quote_plus("7926COAco87") + "@cluster0.zmj8z.mongodb.net/partyCodes?retryWrites=true&w=majority")
 db = client['mydatabase']
 codes = db['partyCodes']
 
