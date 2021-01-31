8d822295d5533906aadf56cd361bcc1170fdb96f
Faraz Murshed <farazmurshed@gmail.com>
2021-01-30 23:36:36 -0800

    test party codes - fixed stoi

--- prompter.py
+++ prompter.py
@@ -29,7 +29,7 @@ def data():
 def handle_sms():
     # store incoming message
     incoming = request.values.get('Body', None)
-
+    sc.generate_code()
     # count messages in session
     counter = session.get('counter', 0)
 
